096f903049bda3124582f52f94532762e08c8f7a
nicolascaliari <nicolascaliari28@gmail.com>
2024-01-22 11:34:21 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://iselec-backend-production.up.railway.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: '*',
