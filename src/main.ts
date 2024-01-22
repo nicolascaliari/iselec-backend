1b7f7c203732d8d8fa1a55f6a9d78f007348405b
nicolascaliari <nicolascaliari28@gmail.com>
2024-01-22 15:08:50 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 
 const bodyParser = require('body-parser');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
+
+
+  const corsOptions: CorsOptions = {
     origin: "http://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-  });
+  };
 
-  // Configurar el body-parser
+  app.enableCors(corsOptions);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  // Agregar el Validación Global
-  app.useGlobalPipes(new ValidationPipe());
+
+
 
   const configService = app.get(ConfigService);
 
